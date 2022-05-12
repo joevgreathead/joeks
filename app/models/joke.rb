6f165f2b1c9a561925b887b4c20bbaf011df6792
joevgreathead <joevgreathead@gmail.com>
2022-05-12 00:12:19 -0700

    Add visual and layout updates for readability

--- app/models/joke.rb
+++ app/models/joke.rb
@@ -5,12 +5,18 @@ class Joke < ApplicationRecord
   has_many :joke_categories
   has_many :categories, through: :joke_categories
 
+  before_save :trim_joke
+
   def display_title
     self.title.present? ? self.title : "Joke ##{self.id}"
   end
 
   def self.next_id
     maximum(:id)&.next || 1
+  end
+
+  def trim_joke
+    self.joke.strip!
   end
 
 end
