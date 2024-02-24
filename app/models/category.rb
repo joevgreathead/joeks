5206fa565c711aded1a679647a0f67758b395965
joevgreathead <joevgreathead@gmail.com>
2024-02-23 19:30:43 -0500

    Update validations on models

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,8 +2,12 @@ class Category < ApplicationRecord
   has_many :joke_categories, dependent: :destroy
   has_many :jokes, through: :joke_categories
 
-  before_create :add_color
   after_initialize :add_color
+
+  validates :title, presence: true
+  validates :color, presence: true
+  validates :color, format: { with: /#\w{6}/, message: "must be in format #A1B2C3" }
+  validates :title, uniqueness: true
 
   def add_color
     self.color = "#" + ("%06x" % (rand * 0xffffff)) if self.color.blank?
--- app/models/joke.rb
+++ app/models/joke.rb
@@ -5,7 +5,15 @@ class Joke < ApplicationRecord
   has_many :joke_categories
   has_many :categories, through: :joke_categories
 
+  validates :title, presence: true
+  validates :joke, presence: true
+
   before_save :trim_joke
+  before_validation :add_title
+
+  def add_title
+    self.title = "Joke ##{Joke.maximum(:id).next}" unless self.title.present?
+  end
 
   def display_title
     self.title.present? ? self.title : "Joke ##{self.id}"
